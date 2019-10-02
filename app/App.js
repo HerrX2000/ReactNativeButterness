d5d88dc8c8b5596eebdb2900f0dcadec76c2eadf
FREDDY-DESKTOP\Freddy <youfreddi@gmail.com>
2019-10-02 22:06:15 +0200

    Updated React Native from 0.59 to 0.61.1 Dependencies updated. The app is now always referred  as Butterness.

--- app/App.js
+++ app/App.js
@@ -10,13 +10,22 @@ BottomTabBar
 
 import {
   createAppContainer,
-  createStackNavigator,
   createTabNavigator,
-  createBottomTabNavigator,
   createMaterialTopTabNavigator,
+  
+} from 'react-navigation';
+
+import {
   createDrawerNavigator,
   DrawerActions
-} from 'react-navigation';
+} from 'react-navigation-drawer'
+
+import {
+  createStackNavigator
+} from 'react-navigation-stack';
+import {
+  createBottomTabNavigator,
+} from 'react-navigation-tabs';
 
 import { Icon } from 'react-native-elements'
 
