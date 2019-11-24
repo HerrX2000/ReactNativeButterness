1e2693b4fbd745187334425a98e0be8deb09684f
FREDDY-DESKTOP\Freddy <youfreddi@gmail.com>
2019-11-24 19:23:24 +0100

    removed impressum

--- app/components/Impressum.js
+++ app/components/Impressum.js
@@ -51,12 +51,8 @@
             <View style={{flex: 1, flexDirection:'column'}}>
                 <View style={{flex:1,padding:10}}>
                     <Text>Die App wird entwickelt von Frederik Mann.</Text>
-                    <Text>Diese Seite wird betrieben durch eine einfache Gesellschaft(Art. 530 &amp; 551, schweizer OR) mit Sitz in Zug(CH)</Text>
                     <Text style={{paddingTop:5,fontWeight:"bold"}}>Allgemeine Informationen</Text>
-                    <Text style={{paddingTop:5,fontStyle: 'italic'}}>Name &amp; Anschrift:</Text>
-                    <Text>Flavio Kleiber</Text>
-                    <Text>Neugasse 8</Text>
-                    <Text>6300 Zug</Text>
+                    <Text style={{paddingTop:5,fontStyle: 'italic'}}>Name &amp; Anschrift:</Text>       
                     <Text style={{paddingTop:5,fontStyle: 'italic'}}>E-Mail Adresse:</Text>
                     <Text selectable={true} onPress={() => Interactions.onPressLink("mailto:"+constants.contactMail)}>{constants.contactMail}</Text>
                     <Text style={{paddingTop:5,fontStyle: 'italic'}}>Datenschutzerklärung:</Text>
